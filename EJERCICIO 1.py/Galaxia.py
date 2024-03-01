78f2be3cf67f551078ccb1794f7d4a752728d8b7
miserranoleo <mserrleo@myuax.com>
2024-03-01 10:32:02 +0100

    cambios

---
+++ EJERCICIO 1.py/Galaxia.py
@@ -0,0 +1,7 @@
+def imprimir_informacion(estrella):
+    print(estrella)
+    print(f"Galaxia: {estrella.galaxia()}")
+
+def calcular_y_mostrar_distancias(estrella1, estrella2):
+    distancia = estrella1.distancia(estrella2)
+    print(f"Distancia entre {estrella1.nombre} y {estrella2.nombre}: {distancia:.2f}")
